6b4e017add1bff7cd791e53177b54e7ba864f7be
Amandeep Singh <shampy@Amandeeps-MacBook-Air.local>
2026-07-07 02:34:26 +0530

    Fix deprecated dynamodb_table parameter

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,12 +8,12 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "miratech-optum-terraform-state"
-    key            = "amazon-connect-chat/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
+backend "s3" {
+  bucket       = "miratech-optum-terraform-state"
+  key          = "amazon-connect-chat/terraform.tfstate"
+  region       = "us-east-1"
+  encrypt      = true
+  use_lockfile = true          # Replaces deprecated dynamodb_table
   }
 }
 
